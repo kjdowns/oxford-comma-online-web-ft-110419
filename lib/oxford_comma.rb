9d68c6cfd3c7e1aa3d1bb3158ef0e225081a6969
Kevin Downs <kevin.downs04@gmail.com>
2019-11-05 19:24:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
+  if array.length == 1 
+    return array[0]
+  end
   last = array.pop
   string = array.join(", ")
-  string << " and #{last}"
+  string << ", and #{last}"
 end
